77e68240c26e6b309dd0b89c7247b7134f8861bf
Forwhfang <945338423@qq.com>
2018-08-31 22:34:49 +0800

    Add a spider to crawl Jd

--- Jd_Spider.py
+++ Jd_Spider.py
@@ -2,91 +2,124 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import selenium.common.exceptions
 import json
 import csv
+import time
 
-count = 0
 
-file_format = input('请输入文件保存格式（txt、json、csv）：')
-while file_format!='txt' and file_format!='json' and file_format!='csv':
-    file_format = input('输入错误，请重新输入文件保存格式（txt、json、csv）：')
-if file_format=='txt' :
-    file = open('Jd.txt','w',encoding='utf-8')
-elif file_format=='json' :
-    file = open('Jd.json','w',encoding='utf-8')
-elif file_format=='csv' :
-    file = open('Jd.csv','w',encoding='utf-8',newline='')
-    writer = csv.writer(file)
+class JdSpider():
+    def init(self):        
+        self.file_format = input('请输入文件保存格式（txt、json、csv）：')
+        while self.file_format!='txt' and self.file_format!='json' and self.file_format!='csv':
+            self.file_format = input('输入错误，请重新输入文件保存格式（txt、json、csv）：')
+        if self.file_format=='txt' :
+            self.file = open('Jd.txt','w',encoding='utf-8')
+        elif self.file_format=='json' :
+            self.file = open('Jd.json','w',encoding='utf-8')
+        elif self.file_format=='csv' :
+            self.file = open('Jd.csv','w',encoding='utf-8',newline='')#
+            self.writer = csv.writer(self.file)
+        print('File Initialized')
 
-browser = webdriver.Chrome()
-browser.implicitly_wait(10)
-wait = WebDriverWait(browser,10)
-start_url = 'https://search.jd.com/Search?keyword=%E7%AC%94%E8%AE%B0%E6%9C%AC&enc=utf-8&wq=%E7%AC%94%E8%AE%B0%E6%9C%AC&pvid=e8604bd75a024b31a9aff06b803229ea'
-browser.get(start_url)
-browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        self.prices = []
+        self.names = []
+        self.commits = []
+        self.count = 0
+        self.start_url = 'https://search.jd.com/Search?keyword=%E7%AC%94%E8%AE%B0%E6%9C%AC&enc=utf-8&pvid=c4fa7a2ce8f64c8dbe121a5edac4add2'
+        print('Data Initialized')
 
-print('Processing')
-while True :
-    try:
-        symbol = wait.until(EC.presence_of_element_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[2]/strong/i')))
-        symbol = wait.until(EC.presence_of_element_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[3]/a/em')))
-        symbol = wait.until(EC.presence_of_element_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[4]/strong')))
-        #symbol = wait.until(EC.presence_of_element_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[5]/span/a')))
-    except selenium.common.exceptions.TimeoutException:
-        continue
-    else:
-        prices = browser.find_elements_by_xpath('//div[@class="gl-i-wrap"]/div[2]/strong/i')
-        names = browser.find_elements_by_xpath('//div[@class="gl-i-wrap"]/div[3]/a/em')
-        commits = browser.find_elements_by_xpath('//div[@class="gl-i-wrap"]/div[4]/strong')
-        #shops = browser.find_elements_by_xpath('//div[@class="gl-i-wrap"]/div[5]/span/a')
-        
-    if file_format=='txt' :
-        for i in range(len(prices)):
-            count += 1
-            print(count)
-            file.write('--------------------'+str(count)+'--------------------\n')
-            file.write('price：')
-            file.write(prices[i].text)
-            file.write('\n')
-            file.write('name：')
-            file.write(names[i].text)
-            file.write('\n')
-            file.write('commit：')
-            file.write(commits[i].text)
-            file.write('\n')
-            #file.write('shop：')
-            #file.write(shops[i].text)
-            #file.write('\n')
-    elif file_format=='json' :
-        for i in range(len(prices)):
-            count += 1
-            print(count)
-            item = {}
-            item['price'] = prices[i].text
-            item['name'] = names[i].text
-            item['commit'] = commits[i].text
-            #item['shop'] = shops[i].text
-            json.dump(item,file,ensure_ascii = False)
-    elif file_format=='csv' :
-        for i in range(len(prices)):
-            count += 1
-            print(count)
-            item = {}
-            item['price'] = prices[i].text
-            item['name'] = names[i].text
-            item['commit'] = commits[i].text
-            #item['shop'] = shops[i].text
-            for key in item:
-                writer.writerow([key, item[key]])
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(10)
+        self.wait = WebDriverWait(self.browser,10)
+        print('Browser Initialized')
     
-    try:
-        next_page = browser.find_element_by_xpath('//a[@class="pn-next" and @onclick]')
-    except selenium.common.exceptions.NoSuchElementException:
-        break
-    else:
-        next_page.click()
-        browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    def parse_page(self):
+        try:
+            self.prices = self.wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[2]/strong/i')))
+            self.names = self.wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[3]/a/em')))
+            self.commits = self.wait.until(EC.presence_of_all_elements_located((By.XPATH,'//div[@class="gl-i-wrap"]/div[4]/strong')))
+        except selenium.common.exceptions.TimeoutException:
+            print('parse_page:TimeoutException')
+            self.parse_page()
+        except selenium.common.exceptions.StaleElementReferenceException:
+            print('parse_page:StaleElementReferenceException')
+            self.browser.refresh()
+        else:
+            if len(self.prices)!=60 or len(self.names)!=60 or len(self.commits)!=60:
+                print('Trying...')
+                self.parse_page()
 
-file,close()
-browser.quit()
-print('Finished')
+    def write_to_file(self):
+        try:
+            if self.file_format=='txt' :
+                for i in range(60):
+                    self.count += 1
+                    print('录入数据：'+str(self.count))
+                    self.file.write('--------------------'+str(self.count)+'--------------------\n')
+                    self.file.write('price：')
+                    self.file.write(self.prices[i].text)
+                    self.file.write('\n')
+                    self.file.write('name：')
+                    self.file.write(self.names[i].text)
+                    self.file.write('\n')
+                    self.file.write('commit：')
+                    self.file.write(self.commits[i].text)
+                    self.file.write('\n')
+            elif self.file_format=='json' :
+                for i in range(60):
+                    self.count += 1
+                    print('录入数据：'+str(self.count))
+                    item = {}
+                    item['price'] = self.prices[i].text
+                    item['name'] = self.names[i].text
+                    item['commit'] = self.commits[i].text                
+                    json.dump(item,self.file,ensure_ascii = False)                
+            elif self.file_format=='csv' :
+                for i in range(60):
+                    self.count += 1
+                    print('录入数据：'+str(self.count))
+                    item = {}
+                    item['price'] = self.prices[i].text
+                    item['name'] = self.names[i].text
+                    item['commit'] = self.commits[i].text
+                    for key in item:
+                        self.writer.writerow([key, item[key]])
+        except selenium.common.exceptions.StaleElementReferenceException:
+            print('write_to_file:StaleElementReferenceException')
+            self.browser.refresh()
+
+    def turn_page(self):
+        try:
+            self.wait.until(EC.presence_of_element_located((By.XPATH,'//a[@class="pn-next" and @onclick]'))).click()#
+            self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            time.sleep(3)
+        except selenium.common.exceptions.NoSuchElementException:
+            return True
+        except selenium.common.exceptions.TimeoutException:
+            print('turn_page:TimeoutException')
+            self.page_turning()
+        except selenium.common.exceptions.StaleElementReferenceException:
+            print('turn_page:StaleElementReferenceException')
+            self.browser.refresh()
+        else:
+            return False
+
+    def close(self):
+        self.browser.quit()
+        print('Finished')
+    
+    def crawl(self):
+        self.init()
+        self.browser.get(self.start_url)
+        self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        while True:
+            self.parse_page()
+            self.write_to_file()
+            if self.turn_page()==True :
+                break
+        self.close()
+        
+if __name__ == '__main__':
+    spider = JdSpider()
+    spider.crawl()
